af279f45750a152ae2fb23d86a02af00a0c2b7a6
yangchenghu <yangchenghu@58.com>
2019-12-18 12:20:07 +0800

    提交

--- erya_spider/src/main/java/spider/SpiderApplication.java
+++ erya_spider/src/main/java/spider/SpiderApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 //@EnableAsync(proxyTargetClass = true)
-@MapperScan("cn.ych.erya.spider.mapper")
+@MapperScan("spider.mapper")
 @SpringBootApplication
 public class SpiderApplication {
 
